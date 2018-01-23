51e3457127ad100cb9f95b14aee0d4957af7c58b
Roman Pobotin <rpobotin@adyax.com>
2018-01-23 13:01:40 +0200

    Load test update

--- tests/Performance/locust_test.py
+++ tests/Performance/locust_test.py
@@ -2,48 +2,35 @@ from locust import HttpLocust, TaskSet, task
 
 
 class UserBehavior(TaskSet):
-    access_token = None
-
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
         self.login()
 
     def login(self):
-        response = self.client.post("url", {"username": "admin", "password": "admin"})
-        # response_body = response.json()
-        # self.access_token = response_body.get('access_token')
+        self.client.post('http://store.demoqa.com/wp-login.php', {'log': 'test_user123', 'pwd': '123qwe123'})
 
     @task
-    def get_index(self):
-        self.client.get("/")
+    def get_home(self):
+        self.client.get('/')
 
     @task
-    def get_news(self):
-        self.client.get("/news")
+    def get_products(self):
+        self.client.get('/products-page')
 
     @task
-    def get_tasks(self):
-        self.client.get("/tasks")
+    def get_categories(self):
+        self.client.get('/products-page/product-category')
 
     @task
-    def get_planning(self):
-        self.client.get("/planning")
+    def get_specific_product(self):
+        self.client.get('/products-page/product-category/n')
 
     @task
-    def get_documents(self):
-        self.client.get("/documents")
-
-    @task
-    def get_stores(self):
-        self.client.get("/stores")
-
-    @task
-    def get_supplies(self):
-        self.client.get("/supplies")
+    def get_cart(self):
+        self.client.get('/products-page/checkout')
 
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
-    host = 'url'
+    host = 'http://store.demoqa.com'
