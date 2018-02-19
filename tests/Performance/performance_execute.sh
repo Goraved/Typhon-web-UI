b70446ea25dcef03adce28c4510809bae41fb3c3
Roman Pobotin <rpobotin@adyax.com>
2018-02-19 17:38:16 +0200

    Load tests update

--- tests/Performance/performance_execute.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Open UI server
-open http://127.0.0.1:8089/
-locust -f locust_test.py
-
-## run without UI c = count of users; r = hatch rate;
-#locust -f --no-web -c 1000 -r 100 --run-time 1h30m
