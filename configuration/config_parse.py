bfa8a9b70f9d3b268d069bfb42fe0641f996cf23
Roman Pobotin <Roman.pobotin@gmail.com>
2021-11-01 11:33:20 +0200

    Update BasePage (#12)
    * Update BasePage
    
    * Fix linter
    
    * Fix linter
    
    * Fix remote run
    
    * Fix remote run
    
    * Fix remote run
    
    * Fix remote run

--- configuration/config_parse.py
+++ configuration/config_parse.py
@@ -5,34 +5,35 @@ import yaml
 
 # All configs placed into config/configs.yaml file. Please declare only variables here and set all values in yaml file
 
-CONFIGS = yaml.safe_load(open(f'{os.path.dirname(os.path.abspath(__file__))}/configs.yaml'))
-ENV_CONFIGS = CONFIGS['environments'][os.getenv('ENVIRONMENT', 'dev')]
-URLS = CONFIGS['urls']
-GENERAL = CONFIGS['general']
+with open(f'{os.path.dirname(os.path.abspath(__file__))}/configs.yaml') as CONFIGS:
+    CONFIGS = yaml.safe_load(CONFIGS)
+    ENV_CONFIGS = CONFIGS['environments'][os.getenv('ENVIRONMENT', 'dev')]
+    URLS = CONFIGS['urls']
+    GENERAL = CONFIGS['general']
 
 
-def get_value(key: str, key_type: str, *args):
-    """
-    Get constant value from YAML file by default, or from environment variables if exists
-    """
-    value = os.getenv(key.upper(), key_type[key])
-    if args and not os.getenv(key.upper()):
-        value = value.format(*args)
-    return value
+    def get_value(key: str, key_type: str, *args):
+        """
+        Get constant value from YAML file by default, or from environment variables if exists
+        """
+        value = os.getenv(key.upper(), key_type[key])
+        if args and not os.getenv(key.upper()):
+            value = value.format(*args)
+        return value
 
 
-# Environment settings
-MAIN_UI_URL = get_value('main_ui_url', URLS)
+    # Environment settings
+    MAIN_UI_URL = get_value('main_ui_url', URLS)
 
-PROJECT = get_value('project', GENERAL)
-LINK_TYPE_TEST_CASE = get_value('link_type_test_case', GENERAL)
-LINK_TYPE_LINK = get_value('link_type_link', GENERAL)
-TEST_CASE = get_value('test_case', GENERAL)
-BUG = get_value('bug', GENERAL)
-GITHUB = get_value('git_path', GENERAL)
+    PROJECT = get_value('project', GENERAL)
+    LINK_TYPE_TEST_CASE = get_value('link_type_test_case', GENERAL)
+    LINK_TYPE_LINK = get_value('link_type_link', GENERAL)
+    TEST_CASE = get_value('test_case', GENERAL)
+    BUG = get_value('bug', GENERAL)
+    GITHUB = get_value('git_path', GENERAL)
 
-TIMEOUT_SEC = get_value('timeout_sec', CONFIGS)
-IMPLICIT_SEC = get_value('implicit_sec', CONFIGS)
+    TIMEOUT_SEC = get_value('timeout_sec', CONFIGS)
+    IMPLICIT_SEC = get_value('implicit_sec', CONFIGS)
 
 OS_NAME = platform.system()
 OS_VERSION = platform.version()
--- lib/utilities.py
+++ lib/utilities.py
@@ -3,6 +3,8 @@ import os
 import allure
 import gevent
 import selenium
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 from base_definitions import ROOT_DIR
 from configuration.config_parse import GITHUB, OS_VERSION
@@ -10,13 +12,28 @@ from configuration.config_parse import GITHUB, OS_VERSION
 
 class Utilities:
     @staticmethod
-    def get_screenshot(driver):
-        allure.attach(driver.get_screenshot_as_png(), name='failure_screenshot',
-                      attachment_type=allure.attachment_type.PNG)
+    def get_screenshot(driver: WebDriver):
+        if not driver:
+            return
+        try:
+            allure.attach(driver.get_screenshot_as_png(), name='screenshot', attachment_type=allure.attachment_type.PNG)
+        except WebDriverException:
+            pass  # Session timed out
 
     @staticmethod
-    def get_html_source(driver):
-        allure.attach(driver.page_source, name='html_source', attachment_type=allure.attachment_type.HTML)
+    def get_html_source(driver: WebDriver):
+        if not driver:
+            return
+        try:
+            html = driver.page_source
+            if html:
+                allure.attach(html, name='html_source', attachment_type=allure.attachment_type.HTML)
+            console_log = driver.get_log('browser')
+            if console_log:
+                console_text = '\n\n'.join([f'{_["level"]} - {_["message"]}' for _ in console_log])
+                allure.attach(console_text, name='console_log', attachment_type=allure.attachment_type.TEXT)
+        except WebDriverException:
+            pass  # Session timed out
 
     @staticmethod
     def fix_properties(driver):
@@ -34,14 +51,14 @@ class Utilities:
                         gevent.sleep(wait_interval)  # will be useful in parallel mode
         else:
             os.mkdir(f"{ROOT_DIR}/allure-results")
-        file = open(properties_path, "w+")
-        file.write(f"Environment {os.getenv('ENVIRONMENT', '').upper()}\n")
-        file.write(f"Browser {browser}\n")
-        try:
-            file.write(f"{browser}_VERSION {driver.capabilities['browserVersion']}\n")
-        except KeyError:
-            file.write(f"{browser}_VERSION {driver.capabilities['version']}\n")
-        file.write(f"Git {GITHUB}\n")
-        file.write(f"OS_VERSION {OS_VERSION}\n")
-        file.write(f"SELENIUM_VERSION {selenium.__version__}\n")
-        file.write(f"HEADLESS {os.getenv('HEADLESS')}\n")
+        with open(properties_path, "w+") as file:
+            file.write(f"Environment {os.getenv('ENVIRONMENT', '').upper()}\n")
+            file.write(f"Browser {browser}\n")
+            try:
+                file.write(f"{browser}_VERSION {driver.capabilities['browserVersion']}\n")
+            except KeyError:
+                file.write(f"{browser}_VERSION {driver.capabilities['version']}\n")
+            file.write(f"Git {GITHUB}\n")
+            file.write(f"OS_VERSION {OS_VERSION}\n")
+            file.write(f"SELENIUM_VERSION {selenium.__version__}\n")
+            file.write(f"HEADLESS {os.getenv('HEADLESS')}\n")
