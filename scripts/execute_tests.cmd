fb5af43ff5e2cf9e98719af63f225e6a4e703c09
Goraved <roman.pobotin@gmail.com>
2021-11-16 15:53:15 +0200

    Fix allure folder names

--- scripts/execute_tests.cmd
+++ scripts/execute_tests.cmd
@@ -2,28 +2,28 @@
 cd ..
 for /f "delims=" %%# in ('powershell get-date -format "{dd-MMM-yyyy_HH_mm}"') do @set _now=%%#
 call %~dp0venv\Scripts\activate
-mkdir %~dp0allure_reports\archive\%_now%
+mkdir %~dp0allure-results\archive\%_now%
 
 
 : Run The tests in project folder
-python -m pytest %~dp0tests\UI\  --alluredir %~dp0allure_reports\archive\%_now%
+python -m pytest %~dp0tests\UI\  --alluredir %~dp0allure-results\archive\%_now%
 
 : Environments settings
-copy %~dp0allure_reports\environment.properties %~dp0allure_reports\archive\%_now%
+copy %~dp0allure-results\environment.properties %~dp0allure-results\archive\%_now%
 
 : Copy previous history
-mkdir %~dp0allure_reports\archive\%_now%\history
-copy %~dp0allure_reports\history\*.json %~dp0allure_reports\archive\%_now%\history
+mkdir %~dp0allure-results\archive\%_now%\history
+copy %~dp0allure-results\history\*.json %~dp0allure-results\archive\%_now%\history
 
 : Generate allure report folder
-powershell -command "allure generate %~dp0allure_reports\archive\%_now% -o %~dp0allure_reports\archive\%_now%\generated-report"
+powershell -command "allure generate %~dp0allure-results\archive\%_now% -o %~dp0allure-results\archive\%_now%\generated-report"
 
 : Saving test run to history
-del /q %~dp0allure_reports\history\*
-copy %~dp0allure_reports\archive\%_now%\generated-report\history\*.json %~dp0allure_reports\history
+del /q %~dp0allure-results\history\*
+copy %~dp0allure-results\archive\%_now%\generated-report\history\*.json %~dp0allure-results\history
 
 :Open generated report
-powershell -command "allure open %~dp0allure_reports\archive\%_now%\generated-report"
+powershell -command "allure open %~dp0allure-results\archive\%_now%\generated-report"
 
 : Run server with generated allure report
-:powershell -noexit "allure serve %~dp0allure_reports\archive\%_now%"
+:powershell -noexit "allure serve %~dp0allure-results\archive\%_now%"
--- scripts/execute_tests.sh
+++ scripts/execute_tests.sh
@@ -16,29 +16,32 @@ export PYTHONPATH
 ENVIRONMENT=DEV
 RUN_TESTS=ui
 pip install -r requirements.txt --quiet
+mkdir -p allure-results
+mkdir -p allure-results/archive
+mkdir -p allure-results/history
 
 # Run The tests in project folder
-py.test -n auto --dist=loadfile --tb=no ${dir}/tests/${RUN_TESTS} --alluredir ${dir}/allure_reports/archive/${_now}
+py.test -n auto --dist=loadfile --tb=no ${dir}/tests/${RUN_TESTS} --alluredir ${dir}/allure-results/archive/${_now}
 
 ## Environments settings
-cp ${dir}/allure_reports/environment.properties ${dir}/allure_reports/archive/${_now}
+cp ${dir}/allure-results/environment.properties ${dir}/allure-results/archive/${_now}
 
 ## Copy previous history
-mkdir ${dir}/allure_reports/archive/${_now}/history
-cp ${dir}/allure_reports/history/*.json ${dir}/allure_reports/archive/${_now}/history
+mkdir ${dir}/allure-results/archive/${_now}/history
+cp ${dir}/allure-results/history/*.json ${dir}/allure-results/archive/${_now}/history
 
 ## Generate allure report folder
-allure generate ${dir}/allure_reports/archive/${_now} -o ${dir}/allure_reports/archive/${_now}/generated-report
+allure generate ${dir}/allure-results/archive/${_now} -o ${dir}/allure-results/archive/${_now}/generated-report
 
 ## Saving current test run to history
-rm ${dir}/allure_reports/history/*
-cp -r ${dir}/allure_reports/archive/${_now}/generated-report/history/*.json ${dir}/allure_reports/history
+rm ${dir}/allure-results/history/*
+cp -r ${dir}/allure-results/archive/${_now}/generated-report/history/*.json ${dir}/allure-results/history
 
 ## Run server with generated allure report
-#allure serve ${dir}/allure_reports/archive/${_now}
+#allure serve ${dir}/allure-results/archive/${_now}
 
 ## Open generated report
-allure open ${dir}/allure_reports/archive/${_now}/generated-report
+allure open ${dir}/allure-results/archive/${_now}/generated-report
 
 # Send email with latest run
 #python ${dir}/framework/send_email.py
