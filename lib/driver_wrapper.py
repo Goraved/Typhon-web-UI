480833220425f39d0a5f77b79cbd85bf8012dee4
rpobotin <rpobotin@bcdtriptech.com>
2022-09-29 11:01:25 +0300

    Update selenium w3c

--- lib/driver_wrapper.py
+++ lib/driver_wrapper.py
@@ -20,7 +20,6 @@ class DriverManager:
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_experimental_option("w3c", False)
         options.add_argument("--no-sandbox")
         return options
 
